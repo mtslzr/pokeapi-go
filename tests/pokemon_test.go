06099190ce9f434efbc04eecd91af8bf2907a302
Matthew Salazar <m@tthewsalazar.com>
2019-06-22 03:30:07 -0400

    Added all /pokemon/ endpoints; added Ability/EggGroup unit tests

--- tests/pokemon_test.go
+++ tests/pokemon_test.go
@@ -8,6 +8,66 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestAbility(t *testing.T) {
+	result, err := pokeapi.Ability("1")
+	assert.Equal(t, nil, err,
+		"Expect to not receive an error.")
+	assert.IsType(t, &structs.Ability{}, result,
+		"Expect to receive an Ability struct.")
+	assert.Equal(t, "stench", result.Name,
+		"Expect to receive Stench.")
+}
+
+func TestAbilityByName(t *testing.T) {
+	result, err := pokeapi.Ability("stench")
+	assert.Equal(t, nil, err,
+		"Expect to not receive an error.")
+	assert.IsType(t, &structs.Ability{}, result,
+		"Expect to receive an Ability struct.")
+	assert.Equal(t, "stench", result.Name,
+		"Expect to receive Stench.")
+}
+
+func TestAbilityFail(t *testing.T) {
+	result, err := pokeapi.Ability("asdf")
+	assert.NotEqual(t, nil, err,
+		"Expect to receive an error.")
+	assert.IsType(t, &structs.Ability{}, result,
+		"Expect to receive an Ability struct.")
+	assert.Equal(t, "", result.Name,
+		"Expect to receive an empty result.")
+}
+
+func TestEggGroup(t *testing.T) {
+	result, err := pokeapi.EggGroup("1")
+	assert.Equal(t, nil, err,
+		"Expect to not receive an error.")
+	assert.IsType(t, &structs.EggGroup{}, result,
+		"Expect to receive an EggGroup struct.")
+	assert.Equal(t, "monster", result.Name,
+		"Expect to receive Monster.")
+}
+
+func TestEggGroupByName(t *testing.T) {
+	result, err := pokeapi.EggGroup("monster")
+	assert.Equal(t, nil, err,
+		"Expect to not receive an error.")
+	assert.IsType(t, &structs.EggGroup{}, result,
+		"Expect to receive an EggGroup struct.")
+	assert.Equal(t, "monster", result.Name,
+		"Expect to receive Monster.")
+}
+
+func TestEggGroupFail(t *testing.T) {
+	result, err := pokeapi.EggGroup("asdf")
+	assert.NotEqual(t, nil, err,
+		"Expect to receive an error.")
+	assert.IsType(t, &structs.EggGroup{}, result,
+		"Expect to receive am EggGroup struct.")
+	assert.Equal(t, "", result.Name,
+		"Expect to receive an empty result.")
+}
+
 func TestPokemon(t *testing.T) {
 	result, err := pokeapi.Pokemon("1")
 	assert.Equal(t, nil, err,
