74b3fe1f06b22bf8972e0098e6ceac3c38c21e00
augustus <augustus@localhost.localdomain>
2020-11-09 21:51:28 +0000

    Allow setting of remote host
    - For those hosting their own API server this would seem useful.
    - Added retry logic / backoff to http.GET requests
    - Believe this will also fix issue #29 (since we now check
    status codes and raise accordingly)

--- client.go
+++ client.go
@@ -2,19 +2,103 @@ package pokeapi
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"time"
 
 	"github.com/patrickmn/go-cache"
 )
 
-const apiurl = "https://pokeapi.co/api/v2/"
+const (
+	defaultAPIScheme = "https"
+	defaultAPIHost   = "pokeapi.co"
+	defaultAPIUrl    = "/api/v2/"
 
-var c *cache.Cache
+	httpRetries = 3
+)
+
+var (
+	c *cache.Cache
+
+	apiHost   = defaultAPIHost
+	apiScheme = defaultAPIScheme
+)
 
 func init() {
 	c = cache.New(defaultCacheSettings.MinExpire, defaultCacheSettings.MaxExpire)
+}
+
+// SetAPIPath configures the Host and Scheme sections used when requesting data.
+func SetAPIPath(path string) error {
+	u, err := url.Parse(path)
+	if err != nil {
+		return err
+	}
+
+	if u.Host == "" {
+		return fmt.Errorf("failed to parse url host: %s", u.Host)
+	}
+	apiHost = u.Host
+
+	apiScheme = u.Scheme
+	if apiScheme == "" {
+		apiScheme = defaultAPIScheme
+	}
+
+	return nil
+}
+
+// retryableGet performs a HTTP GET on the given URL path. Retries are attempted
+// with backoff for server side errors.
+func retryableGet(resource string) ([]byte, error) {
+	var lasterr error
+
+	for attempt := 0; attempt < httpRetries; attempt++ {
+		time.Sleep(time.Second * time.Duration(attempt*attempt)) // exponential backoff
+
+		req, err := http.NewRequest(http.MethodGet, resource, nil)
+		if err != nil {
+			lasterr = err
+			continue
+		}
+		client := &http.Client{Timeout: 10 * time.Second}
+
+		resp, err := client.Do(req)
+		if err != nil {
+			lasterr = err
+			continue
+		}
+		if resp.Body != nil { // body can be nil if an err status is returned
+			defer resp.Body.Close()
+		}
+
+		if resp.StatusCode >= 500 {
+			lasterr = fmt.Errorf("server returned error (code: %d)", resp.StatusCode)
+			continue
+		} else if resp.StatusCode >= 400 {
+			// if we got a 4xx code then screwed up, no sense retrying ..
+			return nil, err
+		}
+		// since the go client handles redirects (3xx) codes, we'll leave those out
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			lasterr = err
+			continue
+		}
+
+		return body, err
+	}
+
+	return nil, lasterr
+}
+
+// buildURL creates a URL for the given endpoint using the current configuration
+func buildURL(endpoint string) string {
+	apiurl := &url.URL{Scheme: apiScheme, Host: apiHost, Path: defaultAPIUrl + endpoint}
+	return apiurl.String()
 }
 
 func do(endpoint string, obj interface{}) error {
@@ -23,19 +107,7 @@ func do(endpoint string, obj interface{}) error {
 		return json.Unmarshal(cached.([]byte), &obj)
 	}
 
-	req, err := http.NewRequest(http.MethodGet, apiurl+endpoint, nil)
-	if err != nil {
-		return err
-	}
-	client := &http.Client{Timeout: 10 * time.Second}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := retryableGet(buildURL(endpoint))
 	if err != nil {
 		return err
 	}
--- client_test.go
+++ client_test.go
@@ -2,6 +2,8 @@ package pokeapi
 
 import (
 	"encoding/json"
+	"fmt"
+	"net/url"
 	"testing"
 	"time"
 
@@ -11,6 +13,46 @@ import (
 
 var endpoint = "machine?offset=0&limit=1"
 var mockResource structs.Resource
+
+func TestBuildURL(t *testing.T) {
+	scheme := "http"
+	host := "test.io"
+
+	err := SetAPIPath(fmt.Sprintf("%s://%s", scheme, host))
+
+	assert.Nil(t, err)
+
+	assert.Equal(t, host, apiHost)
+	assert.Equal(t, scheme, apiScheme)
+}
+
+var dataTestSetAPIPath = []struct {
+	Given        string
+	ExpectHost   string
+	ExpectScheme string
+	ShouldErr    bool
+}{
+	{"http://foobar.com", "foobar.com", "http", false},
+	{"https://zip.co", "zip.co", "https", false},
+	{"whatisthis.com/", "whatisthis.com", "", true},
+	{(&url.URL{Scheme: defaultAPIScheme, Host: defaultAPIHost}).String(), defaultAPIHost, defaultAPIScheme, false},
+}
+
+func TestAPIPath(t *testing.T) {
+	for _, tt := range dataTestSetAPIPath {
+		t.Run(tt.Given, func(t *testing.T) {
+			err := SetAPIPath(tt.Given)
+
+			assert.Equal(t, tt.ShouldErr, err != nil)
+			if err != nil {
+				return
+			}
+
+			assert.Equal(t, tt.ExpectHost, apiHost)
+			assert.Equal(t, tt.ExpectScheme, apiScheme)
+		})
+	}
+}
 
 func TestSetCache(t *testing.T) {
 	_, found1 := c.Get(endpoint)
